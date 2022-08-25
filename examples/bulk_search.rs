7a27e58358a5d8f53da30bb411ae1a720828e547
Hugo CHASTEL <hugo.chastel@orange.com>
2022-08-25 15:31:25 +0200

    GH-4 # Improve bulk search example

--- examples/bulk_search.rs
+++ examples/bulk_search.rs
@@ -1,4 +1,8 @@
-use std::env;
+use std::{env, io, thread};
+use std::sync::mpsc;
+use std::sync::mpsc::TryRecvError;
+use std::time::{Duration, Instant};
+use std::io::Write;
 use ocd_datalake_rs::{ATOM_VALUE_QUERY_FIELD, Datalake, DatalakeSetting};
 
 fn main() {
@@ -13,7 +17,31 @@ fn main() {
     );
 
     let query_hash = "fbecd3d440a7d439a2a1fd996c703a8d".to_string();  // IPs updated the last day
-    let res = dtl.bulk_search(query_hash, vec![ATOM_VALUE_QUERY_FIELD.to_string()]);
+
+    let (tx, rx) = mpsc::channel();
+    let start_time = Instant::now();
+    let ui_thread = thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(500));
+        print!("{esc}c", esc = 27 as char);  // Clear output
+        match rx.try_recv() {
+            Ok(_) | Err(TryRecvError::Disconnected) => {
+                println!("Bulk search process is done ✅");
+                thread::sleep(Duration::from_secs(1));
+                break;
+            }
+            Err(TryRecvError::Empty) => {
+                print!("Waiting for bulk search completion since {}s", start_time.elapsed().as_secs());
+                io::stdout().flush().unwrap();
+            }
+        }
+    });
+    let bulk_search_thread = thread::spawn(move ||
+        dtl.bulk_search(query_hash, vec![ATOM_VALUE_QUERY_FIELD.to_string()])
+    );
+    let res = bulk_search_thread.join().expect("Thread failed");
+    tx.send(()).unwrap();  // Send the stop signal to ui thread
+    ui_thread.join().unwrap();
+
     match res {
         Ok(atom_values) => println!("{atom_values}"),
         Err(err) => {
